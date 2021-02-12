7e8bcb23fe945142cd867616799d40b7389384b8
MB-NectarSpot <madhuri.b@iim-jobs.com>
2021-02-12 11:29:36 +0530

    php file is added

---
+++ individual-certifications.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="description" content="NectarSpot’s services include continuous evaluation and optimization of web and mobile application(s) goals and increasing efficiency at each stage of sales funnel." />
+    <meta property="og:description" content="NectarSpot’s services include continuous evaluation and optimization of web and mobile application(s) goals and increasing efficiency at each stage of sales funnel." />
+    <title>Individual Certifications | NectarSpot</title>
+    <?php include '../includes/snippets/meta-data.php'; ?>
+   <style>
+        <?php include '../includes/css/bootstrap.min.css'; ?>
+        <?php include '../includes/css/fontawesome.css'; ?>
+        <?php include '../includes/css/montserrat.css'; ?>
+        <?php include '../includes/css/homev8.css'; ?>
+        <?php include '../includes/css/animate.css'; ?>
+        <?php include '../includes/css/header-footer-updated.css'; ?>
+        <?php include '../includes/css/owl.carousel.min.css'; ?>
+        <?php include '../includes/css/owl.theme.default.min.css'; ?>
+        .mb-40 {
+    margin-bottom: 40px;
+}
+        .about-section {
+            padding-bottom: 1em !important;
+        }
+        .service-grid-2 .fixed-header-part {
+           display: none !important;
+       }
+        .service-grid-2 .service-inner-box:hover .pr {
+            background-color: #f7fafe
+        }
+        .service-text-box h3 {
+    color: var(--main-dark-blue-color);
+    font-size: 20px;
+    font-weight: 700;
+}
+.service-grid-2 .cro-list a {
+color: var(--main-dark-blue-color);
+}
+.service-inner-box img {
+    margin: 1em 1em
+}
+.certified-users {
+    font-size: 16px;
+    margin: 0;
+    padding: 0 1em;
+}
+.service-inner-box .cro-list li::before{
+    content: '\f30b' !important;
+}
+.service-inner-box:hover .cro-list a {
+color: var(--main-white-color);
+text-decoration: underline !important;
+}
+.service-inner-box:hover .cro-list li::before, .service-inner-box:hover  .service-text-box h3 {
+color: var(--main-white-color);
+}
+
+.service-grid-2 .service-text-box {
+    z-index: 1;
+}
+
+@media(max-width: 991px) {
+    .mb-40 {
+    margin-bottom: 60px
+}
+}
+
+    </style>
+<?php include "../includes/snippets/under-head-tag.php" ?>
+</head>
+
+<body class="service-grid-2 body">
+    <?php include "../includes/snippets/header-updated.php" ?>
+    <div class="about-section">
+        <div class="container-fluid full-wrapper">
+            <center><h1 class="main-heading blue-font"><mark>Individual</mark> Certifications</h1></center>
+            <div class="ptb-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-12 mb-40">
+					<div class="service-inner-box wow fadeInLeft" data-wow-duration="1500ms">
+						<div class="pr">
+						<img width="196" height="64" src="/images/scrumstudy-logo.png" alt="image">
+                    <p class="certified-users">3 Certified Users</p>    
+                    </div>
+						<div class="opacity-inner-box">
+                        <div class="shape-one"></div>
+							<div class="shape-two"></div>
+							<div class="shape-three"></div>
+						<div class="service-text-box">
+                           
+                        <h3 class="sub-sub-head blue-font">Scrum Study</h3>
+                           <p>SCRUMstudy™ is owned by VMEdu, Inc - a company registered in Frederick, Maryland, USA, with its primary office in Phoenix, Arizona, USA. </p>
+                           <!-- <ul class="cro-list">
+                                <li><p>Advertising on Google</p></li>
+                                <li><p>An online marketing plan</p></li>
+                                <li><p>Advanced AdWords Support</p></li>
+                                
+                           </ul> -->
+                       </div>
+                      
+</div>
+                </div>
+                </div> 
+
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-40">
+					<div class="service-inner-box wow fadeInLeft" data-wow-duration="1500ms">
+						<div class="pr">
+						<img width="196" height="64" src="/images/html-css.jpg" alt="image">
+                        <p class="certified-users">2 Certified Users</p>     
+                    </div>
+						<div class="opacity-inner-box">
+                        <div class="shape-one"></div>
+							<div class="shape-two"></div>
+							<div class="shape-three"></div>
+						<div class="service-text-box">
+                        <h3 class="sub-sub-head blue-font">HTML & CSS</h3>
+                           <p>HTML and CSS are essential skills to have because they are foundational languages of the web.</p>
+                           <!-- <ul class="cro-list">
+                                <li><p>Advertising on Google</p></li>
+                                <li><p>Mobile and Video</p></li>
+                                <li><p>Advanced AdWords Support</p></li>
+                                
+                           </ul> -->
+                        </div>
+                       
+					</div>
+                </div>
+                </div>
+                
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-40">
+					<div class="service-inner-box wow fadeInLeft" data-wow-duration="1500ms">
+						<div class="pr">
+						<img width="196" height="64" src="/images/shopify-logo.svg" alt="image">
+                        <p class="certified-users">2 Certified Users</p>    
+                    </div>
+						<div class="opacity-inner-box">
+                        <div class="shape-one"></div>
+							<div class="shape-two"></div>
+							<div class="shape-three"></div>
+						<div class="service-text-box">
+                        <h3 class="sub-sub-head blue-font">Shopify</h3>
+                           <p>One platform with all the e-commerce features you need to start and grow your business. Use a single dashboard to manage orders, shipping, and payments anywhere you go.</p>
+                           
+                        </div>
+                      
+					</div>
+                </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-40">
+					<div class="service-inner-box wow fadeInLeft" data-wow-duration="1500ms">
+						<div class="pr">
+						<img width="48" height="48" src="/images/responsive-web-design.png" alt="image">
+                    <p class="certified-users">1 Certified User</p>    
+                    </div>
+						<div class="opacity-inner-box">
+                        <div class="shape-one"></div>
+							<div class="shape-two"></div>
+							<div class="shape-three"></div>
+						<div class="service-text-box">
+                           
+                        <h3 class="sub-sub-head blue-font">Web-Responsive</h3>
+                           <p>Responsive Web Design is about using HTML and CSS to automatically resize, hide, shrink, or enlarge, a website, to make it look good on all devices (desktops, tablets, and phones)</p>
+                          
+                       </div>
+                      
+</div>
+                </div>
+                </div> 
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-40">
+					<div class="service-inner-box wow fadeInLeft" data-wow-duration="1500ms">
+						<div class="pr">
+						<img width="48" height="48" src="/images/search-icon.png" alt="image">
+                    <p class="certified-users">2 Certified User</p>    
+                    </div>
+						<div class="opacity-inner-box">
+                        <div class="shape-one"></div>
+							<div class="shape-two"></div>
+							<div class="shape-three"></div>
+						<div class="service-text-box">
+                           
+                        <h3 class="sub-sub-head blue-font">Google Ads Search</h3>
+                           <p>This certification demonstrates proficiency in search advertising, including best practices for creating, managing, and optimizing AdWords search campaigns.</p>
+                          
+                       </div>
+                      
+</div>
+                </div>
+                </div> 
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-40">
+					<div class="service-inner-box wow fadeInLeft" data-wow-duration="1500ms">
+						<div class="pr">
+						<img width="48" height="48" src="/images/display-icon.png" alt="image">
+                        <p class="certified-users">2 Certified Users</p>    
+                    </div>
+						<div class="opacity-inner-box">
+                        <div class="shape-one"></div>
+							<div class="shape-two"></div>
+							<div class="shape-three"></div>
+						<div class="service-text-box">
+                        <h3 class="sub-sub-head blue-font">Google Ads Display</h3>
+                           <p>This certification demonstrates proficiency in display advertising, including best practices for creating, managing, and optimizing AdWords Display campaigns.</p>
+                           
+                        </div>
+                      
+					</div>
+                </div>
+                </div>
+                
+
+              
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+
+
+    <?php include "../includes/snippets/start-your-project-today.php" ?>
+    <?php include "../includes/snippets/footer-updated.php" ?>
+
+
+</body>
+
+</html>
